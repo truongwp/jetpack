2450c5bad078cb67f38db8836e5c8d08cbe14573
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2020-04-30 18:52:35 -0500

    Add new-heights illustration

--- _inc/client/setup-wizard/recommended-features/index.jsx
+++ _inc/client/setup-wizard/recommended-features/index.jsx
@@ -2,9 +2,24 @@
  * External dependencies
  */
 import React from 'react';
+import { translate as __ } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import { imagePath } from 'constants/urls';
+
+import './style.scss';
 
 const RecommendedFeatures = () => {
-	return <p>Hello, world! from the features page</p>;
+	return (
+		<div className="jp-setup-wizard-main">
+			<img
+				src={ imagePath + 'jetpack-new-heights.svg' }
+				alt={ __( 'A rocketship using Jetpack to reach new heights' ) }
+			/>
+		</div>
+	);
 };
 
 export { RecommendedFeatures };
